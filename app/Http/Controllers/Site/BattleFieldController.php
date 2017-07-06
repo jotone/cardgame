01ff6a21bd26702feb89ff1d0dc1466ab092d5f6
jotone <hereyouare1987@gmail.com>
2017-07-06 17:28:08 +0300

    inspiration fix

--- app/Http/Controllers/Site/BattleFieldController.php
+++ app/Http/Controllers/Site/BattleFieldController.php
@@ -744,8 +744,13 @@ class BattleFieldController extends BaseController{
 						if(isset($step_status['actions']['cards'][$player])){
 							unset($step_status['actions']['cards'][$player]);
 						}
+						foreach($step_status['actions']['cards'][$step_status['played_card']['move_to']['player']] as $row){
+							if($row != $step_status['actions']['cards'][$step_status['played_card']['move_to']['row']){
+								unset($step_status['actions']['cards'][$step_status['played_card']['move_to']['player']][$row]);
+							}
+						}
 						$stop = true;
-					break;
+						break;
 					default:
 						if($i == ($n-1)){
 							$step_status['actions']['appear'] = [];
