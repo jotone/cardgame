d5c7c34d107644227302a231d0982316abf28e1c
jotone <hereyouare1987@gmail.com>
2017-07-14 16:37:47 +0300

    admin fix

--- app/Http/Controllers/Admin/AdminActionsController.php
+++ app/Http/Controllers/Admin/AdminActionsController.php
@@ -8,96 +8,96 @@ use Illuminate\Http\Request;
 
 class AdminActionsController extends BaseController
 {
-    public function addAction(Request $request){
-        $data = $request->all();
-        //функция транслитеризации см. app\http\AdminFunctions.php
-        $slug = AdminFunctions::str2url($data['title']);
+	public function addAction(Request $request){
+		$data = $request->all();
+		//функция транслитеризации см. app\http\AdminFunctions.php
+		$slug = AdminFunctions::str2url($data['title']);
 
-        //массив характеристик действий карты
-        $charac = array();
+		//массив характеристик действий карты
+		$charac = array();
 
-        /*
-        * если существует входящий массив характеристик приводим его к виду:
-        * array(
-        *  [порядковый номер характеристики]=>[описание][html]
-        * )
-        */
-        if(isset($data['characteristics'])){
-            $characteristics = json_decode($data['characteristics']);
-            for($i=0; $i<count($characteristics); $i++){
-                if($i%2 == 1){
-                    $charac[] = array($characteristics[$i-1], $characteristics[$i]);
-                }
-            }
-        }
+		/*
+		* если существует входящий массив характеристик приводим его к виду:
+		* array(
+		*  [порядковый номер характеристики]=>[описание][html]
+		* )
+		*/
+		if(isset($data['characteristics'])){
+			$characteristics = json_decode($data['characteristics']);
+			for($i=0; $i<count($characteristics); $i++){
+				if($i%2 == 1){
+					$charac[] = array($characteristics[$i-1], $characteristics[$i]);
+				}
+			}
+		}
 
-        //превращаем массив в строку
-        $charac = serialize($charac);
+		//превращаем массив в строку
+		$charac = serialize($charac);
 
-        //заносим в БД
-        $result = Action::create([
-            'title'         => $data['title'],
-            'slug'          => $slug,
-            'description'   => $data['description'],
-            'html_options'  => $charac
-        ]);
+		//заносим в БД
+		$result = Action::create([
+			'title'			=> $data['title'],
+			'slug'			=> $slug,
+			'description'	=> $data['description'],
+			'html_options'	=> $charac
+		]);
 
-        //если действие занесено в БД, передаем в AJAX запрос success
-        if($result !== false){
-            return 'success';
-        }
-    }
+		//если действие занесено в БД, передаем в AJAX запрос success
+		if($result !== false){
+			return 'success';
+		}
+	}
 
-    public function editAction(Request $request){
-        $data = $request->all();
+	public function editAction(Request $request){
+		$data = $request->all();
 
-        //Находим в БД редактируемое действие карты
-        $editedAction = Action::find($data['id']);
-        if(!empty($editedAction)){
+		//Находим в БД редактируемое действие карты
+		$editedAction = Action::find($data['id']);
+		if(!empty($editedAction)){
 
-            //функция транслитеризации см. app\http\AdminFunctions.php
-            $slug = AdminFunctions::str2url($data['title']);
+			//функция транслитеризации см. app\http\AdminFunctions.php
+			$slug = AdminFunctions::str2url($data['title']);
 
-            //массив характеристик действий карты
-            $charac = array();
+			//массив характеристик действий карты
+			$charac = array();
 
-            /*
-            * если существует входящий массив характеристик приводим его к виду:
-            * array(
-            *  [порядковый номер характеристики]=>[описание][html]
-            * )
-            */
+			/*
+			* если существует входящий массив характеристик приводим его к виду:
+			* array(
+			*  [порядковый номер характеристики]=>[описание][html]
+			* )
+			*/
 
-            if(isset($data['characteristics'])){
-                $characteristics = json_decode($data['characteristics']);
-                for($i=0; $i<count($characteristics); $i++){
-                    if($i%2 == 1){
-                        $charac[] = array($characteristics[$i-1], $characteristics[$i]);
-                    }
-                }
-            }
+			if(isset($data['characteristics'])){
+				$characteristics = json_decode($data['characteristics']);
+				for($i=0; $i<count($characteristics); $i++){
+					if($i%2 == 1){
+						$charac[] = array($characteristics[$i-1], $characteristics[$i]);
+					}
+				}
+			}
 
-            $charac = serialize($charac);
+			$charac = serialize($charac);
 
-            //Изменение данных
-            $editedAction->title        = $data['title'];
-            $editedAction->slug         = $slug;
-            $editedAction->description  = $data['description'];
-            $editedAction->html_options = $charac;
+			//Изменение данных
+			$editedAction->title		= $data['title'];
+			$editedAction->slug			= $slug;
+			$editedAction->description	= $data['description'];
+			$editedAction->html_options	= $charac;
 
-            //Сохранение в БД
-            $result = $editedAction->save();
-            if($result !== false){
-                return 'success';
-            }
-        }
-    }
+			//Сохранение в БД
+			$result = $editedAction->save();
+			if($result !== false){
+				return 'success';
+			}
+		}
+	}
 
-    public function dropAction(Request $request){
-        $dropAction = Action::find($request->input('adm_id'));
-        $result = $dropAction -> delete();
-        if($result !== false){
-            return redirect(route('admin-actions'));
-        }
-    }
+	public function dropAction(Request $request){
+		$dropAction = Action::find($request->input('adm_id'));
+		$result = $dropAction -> delete();
+		if($result !== false){
+			return redirect(route('admin-actions'));
+		}
+	}
 }
