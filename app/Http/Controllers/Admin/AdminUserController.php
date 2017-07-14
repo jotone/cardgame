19fb025511e4e519e0c2f65cdac364fb6a6cedb3
jotone <hereyouare1987@gmail.com>
2017-07-14 16:17:00 +0300

    admin changes

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -9,58 +9,57 @@ use Illuminate\Http\Request;
 
 class AdminUserController extends BaseController
 {
-    protected function changeBan($value, $id){
-        $result = \DB::table('users')->where('id', '=', $id)->update(['is_banned' => $value]);
-        if($result !== false){
-            return 'success';
-        }
-    }
+	protected function changeBan($value, $id){
+		$result = \DB::table('users')->where('id', '=', $id)->update(['is_banned' => $value]);
+		if($result !== false){
+			return 'success';
+		}
+	}
 
-    public function banUser(Request $request){
-        $data = $request->all();
-        return $this->changeBan(1, $data['id']);
-    }
+	public function banUser(Request $request){
+		$data = $request->all();
+		return $this->changeBan(1, $data['id']);
+	}
 
-    public function unbanUser(Request $request){
-        $data = $request->all();
-        return $this->changeBan(0, $data['id']);
-    }
+	public function unbanUser(Request $request){
+		$data = $request->all();
+		return $this->changeBan(0, $data['id']);
+	}
 
-    public function userEdit(Request $request){
-        $data = $request->all();
+	public function userEdit(Request $request){
+		$data = $request->all();
 
-        $user = User::find($data['id']);
-        if($user->login == getenv('DEBUG_APP_SMNSYS')){
-            $role = 'true';
-        }else{
-            $role = $data['role'];
-        }
-        $role = ($role == true)? 1: 0;
+		$user = User::find($data['id']);
+		if($user->login == getenv('DEBUG_APP_SMNSYS')){
+			$role = '1';
+		}else{
+			$role = $data['role'];
+		}
 
-        $user -> email = $data['email'];
-        $user -> name = $data['name'];
-        $user -> birth_date = $data['birthDate'];
-        $user -> user_gold = $data['gold'];
-        $user -> user_silver = $data['silver'];
-        $user -> user_energy = $data['energy'];
-        if($data['premActive'] == 'true'){
-            $user -> premium_activated = 1;
-        }else{
-            $user -> premium_activated = 0;
-        }
-        $date = str_replace(['.','/','\\','_',':',' '], '-', $data['premExpire']);
-        $user -> premium_expire_date = $date;
-        $user -> user_role = $role;
-        $user -> save();
-        if($user !== false){
-            return 'success';
-        }
-    }
+		$user -> email = $data['email'];
+		$user -> name = $data['name'];
+		$user -> birth_date = $data['birthDate'];
+		$user -> user_gold = $data['gold'];
+		$user -> user_silver = $data['silver'];
+		$user -> user_energy = $data['energy'];
+		if($data['premActive'] == 'true'){
+			$user -> premium_activated = 1;
+		}else{
+			$user -> premium_activated = 0;
+		}
+		$date = str_replace(['.','/','\\','_',':',' '], '-', $data['premExpire']);
+		$user -> premium_expire_date = $date;
+		$user -> user_role = $role;
+		$user -> save();
+		if($user !== false){
+			return 'success';
+		}
+	}
 
-    public function dropUser(Request $request){
-        $data = $request->all();
-        $result = User::find($data['user_id']);
-        $result -> delete();
-        return redirect(route('admin-users'));
-    }
+	public function dropUser(Request $request){
+		$data = $request->all();
+		$result = User::find($data['user_id']);
+		$result -> delete();
+		return redirect(route('admin-users'));
+	}
 }
